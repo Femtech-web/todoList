980f87f331c78f8395c95b53835e47a51e81c44f
Adeyemi-Femi <bossfemzy@gmail.com>
2022-12-16 00:11:52 +0100

    Add gitignore, procfile and update port

--- app.js
+++ app.js
@@ -135,6 +135,11 @@ app.post('/', (req, res) => {
         
     })
 
-app.listen(3000, () => {
-    console.log('server started on port 3000')
+    let port = process.env.PORT;
+    if(port == null || port == ""){
+        port = 3000;
+    }
+
+app.listen(port, () => {
+    console.log('Server has Started Sucessfully')
 });
